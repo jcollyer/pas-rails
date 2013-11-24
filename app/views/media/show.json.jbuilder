276612bfd0ffb5b46766b237e654f74283e1ac5d
jcollyer <collyerdesign@gmail.com>
2013-11-24 13:54:50 -0800

    add media scaffold

---
+++ app/views/media/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @medium, :title, :body, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 PasRails::Application.routes.draw do
 
+  resources :media
+
   resources :newsletters
 
   root :to => 'home#index'
