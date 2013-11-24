573188aa0641d84a2051b2a7746ab87ab12097ac
jcollyer <collyerdesign@gmail.com>
2013-11-24 11:11:59 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PasRails::Application.config.secret_key_base = 'da2c314491d468c1234b6e6a42650f4d0b4c1a71816d7ed222fc7777fb732952e9e882e14f86ec5933b64d37ff527121d5688b5c676ffe487fe725b54b004808'
