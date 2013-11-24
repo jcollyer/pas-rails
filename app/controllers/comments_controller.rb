7d139dcdd1c6434291f26385f3b503786db5b6a7
jcollyer <collyerdesign@gmail.com>
2013-11-24 12:25:40 -0800

    update comments controller to rails 4 params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create!(comment_params)
+    redirect_to @post
+  end
+
+
+  private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def comment_params
+      params.require(:comment).permit(:post, :body, :author)
+    end
 end
