3a7d1fd3a232af34a25bec117a425a98cd9cc99f
jcollyer <collyerdesign@gmail.com>
2013-11-26 07:35:18 -0800

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 PasRails::Application.routes.draw do
 
+  root :to => 'home#index'
+
   resources :media
 
   resources :newsletters
-
-  root :to => 'home#index'
 
   resources :posts do
     resources :comments, :only => [:create]
