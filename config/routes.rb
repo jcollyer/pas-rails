b96825dd95a064ec41ef1884a01d6836f622da05
jcollyer <collyerdesign@gmail.com>
2013-11-28 11:05:58 -0800

    add routes for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 PasRails::Application.routes.draw do
 
-  root :to => 'home#index'
+  root :to => 'static#home'
 
   resources :media
 
@@ -9,4 +9,10 @@ PasRails::Application.routes.draw do
   resources :posts do
     resources :comments, :only => [:create]
   end
+
+  get "static/about", as: "about"
+  get "static/contact", as: "contact"
+  get "static/programs", as: "programs"
+  get "static/donate", as: "donate"
 end
+
