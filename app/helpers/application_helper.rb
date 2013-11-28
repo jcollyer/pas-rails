32960ad670bbef171fe157c7fb387be41b63456c
jcollyer <collyerdesign@gmail.com>
2013-11-28 12:40:22 -0800

    add nav_link helper in application

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def nav_link(name, path)
+    if current_page?(path)
+      @title = name
+    end
+    content_tag('li', :class=>(current_page?(path) ? 'active' : nil) ){link_to(name,path)}
+
+  end
 end
