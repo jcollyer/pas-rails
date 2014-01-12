41d35f529a5c907b50414a6f26551c176115d989
jcollyer <collyerdesign@gmail.com>
2014-01-12 13:11:26 -0800

    programs schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131202153850) do
+ActiveRecord::Schema.define(version: 20140112192505) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,13 @@ ActiveRecord::Schema.define(version: 20131202153850) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.string   "author"
+  end
+
+  create_table "programs", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
