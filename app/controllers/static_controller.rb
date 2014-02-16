836f75c0fe8d6aa7c7927ff496783a259550244c
jcollyer <collyerdesign@gmail.com>
2014-02-15 17:37:24 -0800

    update newsletter order on home

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,7 @@
 class StaticController < ApplicationController
   def home
     @posts = Post.last
-    @newsletters = Newsletter.all.order('date ASC')
+    @newsletters = Newsletter.all.order('date DESC')
   end
 
   def about
