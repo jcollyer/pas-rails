90ee3b1efb1abeec10e121222e8ad2a1d102f1d1
jcollyer <collyerdesign@gmail.com>
2014-02-19 07:22:26 -0800

    update comment controller - full CRUD

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,33 @@ class CommentsController < ApplicationController
     redirect_to @post
   end
 
+  # def edit
+  #   @comment = Comment.find(params[:id])
+  #   @post = Post.find(params[:note_id])
+  # end
+
+  # def update
+  #   @comment = Comment.find(params[:comment_id])
+  #   @post = @comment.posts.find(params[:id])
+  #   respond_to do |format|
+  #     if @post.update(post_params)
+  #       format.html { redirect_to posts_path, notice: 'Post was successfully updated.' }
+  #       format.json { head :no_content }
+  #     else
+  #       format.html { render action: 'edit' }
+  #       format.json { render json: @post.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    respond_to do |format|
+      format.html { redirect_to posts_path }
+      format.json { head :no_content }
+    end
+  end
 
   private
     # Never trust parameters from the scary internet, only allow the white list through.
